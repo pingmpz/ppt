3f7d75cdf0285b01ce09150f6a9aac45d594cd62
pingmpz <pingmpz@hotmail.com>
2021-10-18 07:57:19 +0700

    sss

--- pptapp/models.py
+++ pptapp/models.py
@@ -18,17 +18,17 @@ class Order(models.Model):
     date_published = models.DateTimeField(auto_now_add=True)
 
 class Serial(models.Model):
-    code = models.CharField(max_length=20, primary_key=True)
+    id = models.AutoField(primary_key=True)
+    code = models.CharField(max_length=20, default="-")
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    emp_id = models.CharField(max_length=20, default="-") # CREATOR
     date_modified = models.DateTimeField(auto_now=True)
     date_published = models.DateTimeField(auto_now_add=True)
 
 class Path(models.Model):
     id = models.AutoField(primary_key=True)
-    serial = models.ForeignKey(Order, on_delete=models.CASCADE)
+    serial = models.ForeignKey(Serial, on_delete=models.CASCADE)
     emp_id = models.CharField(max_length=20, default="-")  # ACTOR
     location = models.CharField(max_length=100, default="-")
-    status = models.CharField(max_length=20, default="-") # CREATE, REJECT, RECEIVE
+    status = models.CharField(max_length=20, default="-")
     date_modified = models.DateTimeField(auto_now=True)
     date_published = models.DateTimeField(auto_now_add=True)
--- pptapp/urls.py
+++ pptapp/urls.py
@@ -4,8 +4,18 @@ from . import views
 
 urlpatterns = [
     path('index/', views.index, name='index'),
+
     path('new_workorder/', views.new_workorder, name='new_workorder'),
+    path('validate_order_no/', views.validate_order_no, name='validate_order_no'),
+    path('new_workorder_save/', views.new_workorder_save, name='new_workorder_save'),
+
     path('order_master/', views.order_master, name='order_master'),
     path('emp_master/', views.emp_master, name='emp_master'),
     path('fg_master/', views.fg_master, name='fg_master'),
+
+    path('order/<str:order_no>', views.order, name='order'),
+    path('multiple_receive/', views.multiple_receive, name='multiple_receive'),
+    path('serial_receive/', views.serial_receive, name='serial_receive'),
+    path('serial_reject/', views.serial_reject, name='serial_reject'),
+    path('serial_edit/', views.serial_edit, name='serial_edit'),
 ]
